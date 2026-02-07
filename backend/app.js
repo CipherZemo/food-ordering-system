68db29f3a533957a79e12ca0565452fa0b4266b6
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-07 23:35:18 +0530

    mounted auth routes

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use('api/menu', require('./routes/menuRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{ console.log(`Server is running on port ${PORT}` ); })
