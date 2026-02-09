4d63bc702d3b123356d37a2664c41a546d59c98d
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-09 20:57:47 +0530

    profile page

---
+++ customer-frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,12 @@
+const ProfilePage = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-gray-800 mb-8">My Profile</h1>
+      <div className="text-center text-gray-500 py-12">
+        Profile details (will be implemented in later steps)
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
