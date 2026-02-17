0c3c8a2110c70ade75b16eaef3bf9edb9b952256
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-17 14:48:28 +0530

    resolved mongoose pre hook error where no need of next() in async/await

--- backend/models/User.js
+++ backend/models/User.js
@@ -44,11 +44,10 @@ const userSchema = new mongoose.Schema({
 // Hash password before saving
 userSchema.pre('save', async function (next){
     if (!this.isModified('password')){ //this is user document
-        return next();
+        return;
     }
     const salt = await bcrypt.genSalt(10);
     this.password= await bcrypt.hash(this.password,salt);
-    next();
 });
 
 // Method to compare passwords
